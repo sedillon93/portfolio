3e437107339f5f83e8aab775d54cdeef51db5ba4
Shannon Dillon Guha <shannon@Shannons-MacBook-Pro.local>
2017-09-11 16:53:42 -0700

    start to add hamburger menu functionality, wrote constructor function for projects

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var projects = [];
+
+function Project(name, startDate, endDate, description){
+  this.name = name,
+  this.startDate = startDate,
+  this.endDate = endDate,
+  this.description = description,
+  projects.push(this)
+}
+
+new Project('Salmon Cookies', 'August 2017', 'August 2017', 'Prototype business website to track cookie sales at different store locations; Create customer-facing website advertising the business.');

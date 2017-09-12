38d572e25f66ca9048c966c1ba0f763afb0a6e57
Shannon Dillon Guha <shannon@Shannons-MacBook-Pro.local>
2017-09-12 16:18:18 -0700

    revise file structure to be more organized

--- js/work-exp.js
+++ js/work-exp.js
@@ -12,6 +12,7 @@ function Job(name, employer, startDate, endDate, description){
 }
 new Job('Web Developer-in-training', 'Code Fellows', 'August 2017', 'present', 'Learning CSS, HTML, and Javascript to create interactive websites');
 new Job('Elementary Center Instructional Assistant', 'St. Thomas School', 'August 2016', 'June 2017', 'Work with students grades 1-4');
+new Job('Resident Teacher', 'University Child Development School', 'August 2015', 'June 2016', 'Work with students in grades 4-5')
 
 $(document).ready(function(){
   for (var i = 0; i < jobs.length; i++){

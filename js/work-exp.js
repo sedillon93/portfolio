e8e9842e7146efc6af4b00b7f8d86b2b8e7a6a75
Shannon Dillon Guha <shannon@Shannons-MacBook-Pro.local>
2017-09-12 20:54:40 -0700

    add toHtml method for Job object instances

--- js/work-exp.js
+++ js/work-exp.js
@@ -10,14 +10,24 @@ function Job(name, employer, startDate, endDate, description){
   this.description = description,
   jobs.push(this)
 }
-new Job('Web Developer-in-training', 'Code Fellows', 'August 2017', 'present', 'Learning CSS, HTML, and Javascript to create interactive websites');
-new Job('Elementary Center Instructional Assistant', 'St. Thomas School', 'August 2016', 'June 2017', 'Work with students grades 1-4');
-new Job('Resident Teacher', 'University Child Development School', 'August 2015', 'June 2016', 'Work with students in grades 4-5')
+var developer = new Job('Web Developer-in-training', 'Code Fellows', 'August 2017', 'present', 'Learning CSS, HTML, and Javascript to create interactive websites');
+var elementaryIA = new Job('Elementary Center Instructional Assistant', 'St. Thomas School', 'August 2016', 'June 2017', 'Work with students grades 1-4');
+var residentTeacher = new Job('Resident Teacher', 'University Child Development School', 'August 2015', 'June 2016', 'Work with students in grades 4-5')
 
-$(document).ready(function(){
-  for (var i = 0; i < jobs.length; i++){
-    var job = $('<p>' + jobs[i].name + '<br />' + jobs[i].employer + '<br />' + 'From ' + jobs[i].startDate + 'to ' + jobs[i].endDate + '<br />' + jobs[i].description + '</p>');
-    job.addClass('job');
-    $('body').append(job);
-  }
-})
+Job.prototype.toHtml = function(){
+  var $newJob = $('#jobTemplate').clone();
+  $newJob.attr('id', '').show();
+  $newJob.find('#jobTitle')
+              .text(this.name);
+  $newJob.find('#employer')
+              .text(this.employer);
+  $newJob.find('div')
+              .text('From: ' + this.startDate + ' to ' + this.endDate);
+  $newJob.find('#description')
+              .text(this.description);
+  $newJob.appendTo('.jobDisplay')
+}
+
+developer.toHtml();
+elementaryIA.toHtml();
+residentTeacher.toHtml();

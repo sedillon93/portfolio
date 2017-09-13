ca48fb9da28ee49143fadbbd828f4fc648d6e3f0
Shannon Dillon Guha <shannon@Shannons-MacBook-Pro.local>
2017-09-12 17:57:45 -0700

    format toHtml function and attempt at calling the toHtml function

--- js/projects.js
+++ js/projects.js
@@ -11,12 +11,19 @@ function Project(name, startDate, endDate, description, link){
   projects.push(this)
 }
 new Project('Salmon Cookies', 'August 2017', 'August 2017', 'Prototype business website to track cookie sales at different store locations; Create customer-facing website advertising the business.', 'https://sedillon93.github.io/cookie-stand/');
-new Project('Bus Mall focus group', 'August 2017', 'August 2017', 'Collect data on which products customers prefer and generate graph to track preferences', 'https://sedillon93.github.io/bus-mall/')
+new Project('Bus Mall focus group', 'August 2017', 'August 2017', 'Collect data on what products customers prefer and generate graph to track preferences', 'https://sedillon93.github.io/bus-mall/')
 
-$(document).ready(function(){
-  for (var i = 0; i < projects.length; i++){
-    var project = $('<p>' + projects[i].name + '<br />' + projects[i].startDate + ' to ' + projects[i].endDate + '<br />' + projects[i].description + '<br />' + 'Check it out <a href="' + projects[i].link + '">' + 'here' + '</a>' + '</p>');
-    project.addClass('project');
-    $('body').append(project);
-  }
-})
+Project.prototype.toHtml = function(){
+  var $newProject = $('#projectTemplate').clone();
+  $newProject.find('h2')
+                .text(this.name);
+  $newProject.find('#dates')
+                .text(this.startDate + ' to ' + this.endDate);
+  $newProject.find('#description')
+                .text(this.description);
+  $newProject.find('#link a')
+                .html('<a href="' + this.link + '">' + 'here' + '</a>');
+  $newProject.appendTo('#projectDisplay');
+}
+
+$('#projectDisplay').toHtml();

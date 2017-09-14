000893d1f0f51de5d324f4096da9be2720eaff70
Shannon Dillon <sedillon93@gmail.com>
2017-09-14 08:47:37 -0700

    put back content appending/removing with on click function but keep code that allows scrolling, just commented out

--- js/app.js
+++ js/app.js
@@ -4,26 +4,20 @@ $('#hamburgerMenu').on('click', function(){
   $('.navbar').toggle();
 });
 
-$('.navItem').on('click', function(event){
-  var identifier = event.target.id;
-  $('html, body').animate({
-    scrollTop: $('.' + identifier).position().top
-  })
-})
-
-$('#scroll-down').on('click', function(){
-  $('html, body').animate({
-    scrollTop: $('#here').position().top
-  })
-})
-// $(document).ready(function(){
-//   $('li.navItem').children().hide();
+// $('.navItem').on('click', function(event){
+//   var identifier = event.target.id;
+//   $('html, body').animate({
+//     scrollTop: $('.' + identifier).position().top
+//   })
 // })
 
-// $('.navItem').on('click', function(event){
-//   $('li.navItem').children().hide();
-//   var identifier = event.target.id;
-//   $('.' + identifier).show()
-//                       .css({position: 'absolute',
-//                         top: '40vh',
-//                       })
+$(document).ready(function(){
+  $('div.fillNavItem').hide();
+})
+
+$('.navItem').on('click', function(event){
+  $('div.fillNavItem').hide();
+  var identifier = event.target.id;
+  $('.' + identifier).show()
+}
+)

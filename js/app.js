d55c9a4753a3b33dc9a4bea33e2081a5c8795e07
Shannon Dillon <sedillon93@gmail.com>
2017-09-13 17:07:31 -0700

    add event listener to display About Me paragraph on home page when tab is clicked

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 'use strict';
 
-$('#hamburgerMenu').on('click', displayNav);
-
-function displayNav(){
+$('#hamburgerMenu').on('click', function(){
   $('.navbar').toggle();
-}
+});
+
+$('.navItem').on('click', function(event){
+  var identifier = event.target.id;
+  console.log(identifier);
+  $('.' + identifier).toggle();
+})

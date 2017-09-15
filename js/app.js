1f79fb0a10f44f79845769a59fba68a3b040e057
Shannon Dillon <sedillon93@gmail.com>
2017-09-14 21:59:58 -0700

    remove zombie code

--- js/app.js
+++ js/app.js
@@ -4,14 +4,6 @@ $('#hamburgerMenu').on('click', function(){
   $('.navbar').toggle();
 });
 
-//saving this feature for later
-// $('.navItem').on('click', function(event){
-//   var identifier = event.target.id;
-//   $('html, body').animate({
-//     scrollTop: $('.' + identifier).position().top
-//   })
-// })
-
 $(document).ready(function(){
   $('div.fillNavItem').hide();
 })

c26e8aa4ae83bc716647f2325724bf3fa679b35a
Shannon Dillon <sedillon93@gmail.com>
2017-09-15 10:28:02 -0700

    consolidate js logic into app.js file; turn individual declarations of Project instances into array of objects with the project data

--- js/app.js
+++ js/app.js
@@ -14,3 +14,65 @@ $('.navItem').on('click', function(event){
   $('.' + identifier).show()
 }
 )
+
+var projects = [];
+var jobs = [];
+
+function Project(projectTitle, startDate, endDate, description, projectUrl){
+  this.projectTitle = projectTitle,
+  this.startDate = startDate,
+  this.endDate = endDate,
+  this.description = description,
+  this.projectUrl = projectUrl
+}
+
+Project.prototype.toHtml = function(){
+  console.log('working?');
+  var projectHTML = $('#projectTemplate').html();
+  var fillProjectTemplate = Handlebars.compile(projectHTML);
+  this.attr('id', '')
+                .show()
+                .addClass('project');
+  return fillProjectTemplate(this);
+}
+
+projects.forEach(function(project){
+  project.appendTo('#projectDisplay');
+});
+
+function Job(name, employer, startDate, endDate, description){
+  this.name = name,
+  this.employer = employer,
+  this.startDate = startDate,
+  this.endDate = endDate,
+  this.description = description
+}
+
+Job.prototype.render = function(){
+  jobs.push(this);
+}
+
+Job.prototype.toHtml = function(){
+  var $newJob = $('#jobTemplate').clone();
+  $newJob.attr('id', '')
+              .show()
+              .addClass('job');
+  $newJob.find('#jobTitle')
+              .text(this.name);
+  $newJob.find('#employer')
+              .text(this.employer);
+  $newJob.find('div')
+              .text('From: ' + this.startDate + ' to ' + this.endDate);
+  $newJob.find('#description')
+              .text(this.description);
+  $newJob.appendTo('.jobDisplay')
+}
+
+projects.forEach(function(project){
+  project.populateProjectArray();
+});
+
+projects.forEach(function(project){
+  project.toHtml();
+}
+);
--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,17 @@
 'use strict';
-
-var salmonCookies = new Project('Salmon Cookies', 'August 2017', 'August 2017', 'Prototype business website to track cookie sales at different store locations; Create customer-facing website advertising the business.', 'https://sedillon93.github.io/cookie-stand/');
-var busMall = new Project('Bus Mall focus group', 'August 2017', 'August 2017', 'Collect data on what products customers prefer and generate graph to track preferences', 'https://sedillon93.github.io/bus-mall/')
-
-salmonCookies.populateProjectArray();
-busMall.populateProjectArray();
+var projects = [
+  {
+    projectTitle: 'Salmon Cookies',
+    startDate: 'August 2017',
+    endDate: 'August 2017',
+    description: 'Prototype business website to track cookie sales at different store locations; Create customer-facing website advertising the business.',
+    projectUrl: 'https://sedillon93.github.io/cookie-stand/'
+  },
+  {
+    projectTitle: 'Bus Mall focus group',
+    startDate: 'August 2017',
+    endDate: 'August 2017',
+    description: 'Collect data on what products customers prefer and generate graph to track preferences.',
+    projectUrl: 'https://sedillon93.github.io/bus-mall/'
+  }
+];

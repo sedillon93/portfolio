2edd541c1e09cf645d33b60b3d3a21ba345f24e3
Shannon Dillon <sedillon93@gmail.com>
2017-09-16 13:33:50 -0700

    get About Me section to render to page when About tab is clicked

--- js/app.js
+++ js/app.js
@@ -65,15 +65,13 @@ $('.navItem').on('click', function(event){
 })
 
 var renderAboutHTML = function(){
-  var aboutHTML = $('#aboutTemplate').html();
-  var fillAboutTemplate = Handlebars.compile(aboutHTML);
-  fillAboutTemplate({
-    greeting: 'Welcome to my portfolio. Take a look around and be sure to get in touch.',
-    blurb: 'This is a test blurb about myself to see how it renders to the page.'
-  })
-  $('#aboutInfo').addClass('about').append(fillAboutTemplate());
-  console.log(fillAboutTemplate());
-  return fillAboutTemplate;
+  var $aboutHTML = $('#aboutTemplate').html();
+  var $fillAboutTemplate = Handlebars.compile($aboutHTML);
+  var filledTemplate = $fillAboutTemplate({
+    greeting: `Welcome to my portfolio. Take a look around and be sure to get in touch.`,
+    blurb: `This is a test blurb about myself to see how it renders to the page.`
+  });
+  $('#aboutInfo').addClass('about').append(filledTemplate);
 }
 
 renderAboutHTML();

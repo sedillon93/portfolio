04cd1144e62b506ec0d43e4e218cb1bce5c541d8
Shannon Dillon <sedillon93@gmail.com>
2017-09-27 12:34:43 -0700

    add handler for get request to github api through express proxy

---
+++ public/scripts/model/projectRepos.js
@@ -0,0 +1,6 @@
+'use strict';
+var globalApp = globalApp || {};
+
+(function(){
+  //make a get request to github using my token to get all the repos
+})();
--- server.js
+++ server.js
@@ -2,9 +2,17 @@
 
 const express = require('express');
 const app = express();
+const requestProxy = require('express-request-proxy');
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
+
+app.get('/github/*', requestProxy({
+  url: `https://api.github.com/*`,
+  headers: {
+    Authorization: `token ${process.env.GITHUB_TOKEN}`
+  }
+}));
 
 app.get('/', function(request, response){
   response.sendFile('index.html', {root: './public'})
